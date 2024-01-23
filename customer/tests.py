391c05f9de763f6b6960a118fad0903547d92d0a
aditetester <adite.tester@gmail.com>
2024-01-23 18:22:04 +0530

    test case configuration

--- customer/tests.py
+++ customer/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase  # noqa
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Customer
 
-# Create your tests here.
+
+class CustomerRegisterViewTest(APITestCase):
+    def setUp(self):
+        self.client = Customer()
+
+    def test_customer_registration(self):
+        data = {
+            'phone_number': '1234567890',
+            'fcm_token': 'fcm_token',
+            'device_id': 'device_id'
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
