b61c56341f5ae69fbb23b2dc2a50887add8eb97d
aditetester <adite.tester@gmail.com>
2024-02-07 17:24:12 +0530

    add websocket

--- websocket.py
+++ websocket.py
@@ -1,11 +1,33 @@
 import re
 import uuid
-from fastapi import FastAPI, WebSocket, Query, HTTPException
+from fastapi import FastAPI, WebSocket, Query, HTTPException, Depends, Request
 from starlette.middleware.cors import CORSMiddleware
 from typing import Optional
 from starlette.websockets import WebSocketState
 import random
 import uvicorn
+import jwt
+from asgiref.sync import sync_to_async
+from rest_framework import exceptions
+import os
+from django import setup
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hotel_app_backend.settings")
+django.setup()
+
+from hotel.models import Owner
+from customer.models import Customer
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Singleton:
     _instances = {}
@@ -55,12 +77,11 @@ class ConnectionManager(Singleton):
         if websocket.application_state == WebSocketState.CONNECTED:
             await websocket.send_text(message)
 
-    async def send_personal_message(self, message: str, receiver_id: int, user_info, room_id):
+    async def send_personal_message(self, message: str, receiver_id: int, room_id):
         for connection in self.room_clients[room_id]:
             if id(connection) == receiver_id:
-                image_url = user_info.profile_image.url if user_info.profile_image else None
-                combined_message = f"{message} Image URL: {image_url}"
-                await connection.send_text(combined_message)
+                await connection.send_text(message)
+
 
     async def broadcast_to_room(self, room_id: str, message: str):
         for websocket in self.room_clients[room_id]:
@@ -77,23 +98,97 @@ def generate_unique_room_id():
 
 active_rooms = {}
 
+class JWTAuthentication:
+    async def authenticate(self, request, token, user_type):
+        if token:
+            try:
+                payload = jwt.decode(token, 'secret', algorithms=['HS256'])  # Specify algorithms argument
+            except (jwt.DecodeError, jwt.ExpiredSignatureError) as e:
+                print(e)
+                raise exceptions.AuthenticationFailed('Token is invalid')
+        else:
+            raise exceptions.AuthenticationFailed('Token is required')
 
-app = FastAPI()
+        user = None
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+        try:
+            user_id = payload.get('user_id')
+            if user_type == 'owner':
+                user = await self.get_business_owner(user_id)
+                print(user)
+            elif user_type == 'customer':
+                user = await self.get_customer(user_id)
+            else:
+                raise exceptions.AuthenticationFailed('Invalid user_type in token.')
 
+        except Exception as e:
+            print(e)
+            raise exceptions.AuthenticationFailed('Error fetching user from the database')
+
+        if not user:
+            raise exceptions.AuthenticationFailed('User not found.')
+
+        request.user = user
+
+        return (request.user, token)
+    
+    @sync_to_async
+    def get_business_owner(self, user_id):
+        try:
+            return Owner.objects.get(id=user_id)
+        except Owner.DoesNotExist:
+            return None
+
+    @sync_to_async
+    def get_customer(self, user_id):
+        try:
+            return Customer.objects.get(id=user_id)
+        except Customer.DoesNotExist:
+            return None
+    
+async def verify_token(token: str, user_type: str, request: Request = Depends()):
+    try:
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+    except (jwt.DecodeError, jwt.ExpiredSignatureError) as e:
+        print(e)
+        raise HTTPException(status_code=400, detail="Authorization Token is invalid")
+
+    jwt_auth = JWTAuthentication()
+    user, token = await jwt_auth.authenticate(request, token, user_type)
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Authorization Token is invalid")
+
+    return user
+
+async def get_current_user(token: str, user_type: str):
+    """Helper function for auth with Firebase."""
+    if not token:
+        return ""
+    try:
+        user_info = await verify_token(token, user_type)
+        return user_info
+    except HTTPException as e:
+        raise e
+    
 @app.websocket("/ws/room_connection")
 async def room_connection(
     websocket: WebSocket,
     user_type: str = Query(None),
-    room_id: str = Query(None)
+    room_id: str = Query(None),
+    token: str = Query(None)
 ):
+    user_info = await get_current_user(token, user_type)
+    if not user_info:
+        await websocket.close(code=1008, reason="Unauthorized")
+        return
+    
+    if isinstance(user_info, Owner):
+        owner_id = id(websocket)
+        business_owner_id = user_info.id
+        await manager.connect(websocket)
+        
+    
     if user_type == "customer":
         if room_id:
             await websocket.close(code=1008, reason="Room ID should not be provided by customers.")
@@ -111,12 +206,12 @@ async def room_connection(
             await manager.connect(websocket)
             await manager.update_active_room(websocket, room_id)
             await websocket.send_text(f"You are now connected to room {room_id}.")
+            message = f"Student {user_info.hotel_name} entered the room."
+            await manager.send_personal_message(message, owner_id, room_id)
         else:
             await websocket.close(code=1008, reason="Invalid Room ID or Room not found.")
     else:
         await websocket.close(code=1008, reason="Invalid User Type.")
-
-
 
 
 if __name__ == '__main__':
