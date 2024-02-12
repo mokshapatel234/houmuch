3df0df5a17f9abd965f4df4de081320f19b70c92
aditetester <adite.tester@gmail.com>
2024-02-12 17:58:16 +0530

    admin changes

--- hotel/forms.py
+++ hotel/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Property
-from django.utils.safestring import mark_safe
 from django.contrib.gis import forms as gis_forms
 
 
@@ -16,8 +15,5 @@ class PropertyForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PropertyForm, self).__init__(*args, **kwargs)
-        if self.instance and self.instance.image:
-            self.fields['image'].help_text = mark_safe(
-                '<img src="{}" width=280px height=250px/>'.format(f'https://houmuch.s3.amazonaws.com/{self.instance.image}'))
         if 'location' in self.fields:
             self.fields['location'].disabled = True
