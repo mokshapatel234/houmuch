a00b46b5a77e6dfae909439c4d38e953a22e086c
Jaydeep9963 <jikadrajaydeep@gmail.com>
2024-03-27 11:49:49 +0530

    changes for response

--- customer/views.py
+++ customer/views.py
@@ -439,7 +439,8 @@ class BookingRetrieveView(RetrieveAPIView):
             return generate_response(instance, DATA_RETRIEVAL_MESSAGE, status.HTTP_200_OK, self.serializer_class)
         except Http404:
             return error_response(OBJECT_NOT_FOUND_MESSAGE, status.HTTP_400_BAD_REQUEST)
-        except Exception:
+        except Exception as e:
+            print(e)
             return error_response(EXCEPTION_MESSAGE, status.HTTP_400_BAD_REQUEST)
 
 
